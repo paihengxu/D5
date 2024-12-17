a5034c0aac08f9f07573c34cf62afbad700ee1f7
paiheng <xupaiheng@hotmail.com>
2024-12-17 03:43:51 -0500

    test on SimSE dataset & gpt-4o as proposer

--- lm_proposer.py
+++ lm_proposer.py
@@ -1,4 +1,4 @@
-
+import json
 from transformers import AutoTokenizer
 import nltk
 import openai
@@ -12,12 +12,15 @@ import time
 
 openai.api_key = os.environ['openai_key']
 
+client = openai.OpenAI(
+    # This is the default and can be omitted
+    api_key=openai.api_key,
+)
 
 GPT3_TOK = AutoTokenizer.from_pretrained('gpt2-medium')
-SINGLE_SAMPLE_MAX_LENGTH = 256
-MAX_PROMPT_LENGTH = 3200
+SINGLE_SAMPLE_MAX_LENGTH = 1024
+MAX_PROMPT_LENGTH = 128000
 SLEEP_TIME_AFTER_ERROR = 20
-
 
 # the default hypotheses that we use for the prompt
 # if we do not use problem-specific hypotheses
@@ -63,19 +66,37 @@ def prefix_subspan(x: str, prefix_token_max_len: int = SINGLE_SAMPLE_MAX_LENGTH,
 
 
 rm_cmp_prompt = open('templates/rm_cmp_prompt.txt').read()
+
+
 # remove comparison from a text sample x
 def convert_cmp_to_ind(s: str) -> str:
     for _ in range(3):
         if not classify_cmp(s):
             break
         prompt = rm_cmp_prompt.format(input=s)
-        response = gpt3wrapper(prompt=prompt, max_tokens=2048, temperature=0.0, top_p=1, frequency_penalty=0.0, presence_penalty=0.0, stop=['\n\n'], engine='text-davinci-002')
-        if response is None:
-            return s
-        s = response['choices'][0]['text'].strip()
+        # response = gpt3wrapper(prompt=prompt, max_tokens=2048, temperature=0.0, top_p=1, frequency_penalty=0.0, presence_penalty=0.0, stop=['\n\n'], engine='text-davinci-002')
+        # if response is None:
+        #     return s
+        # s = response['choices'][0]['text'].strip()
+        response = client.chat.completions.create(
+            model='gpt-4o',
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            max_completion_tokens=2048,
+            temperature=0.0,
+            top_p=1,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+        s = response.choices[0].message.content
     if classify_cmp(s) or 'group a' in s.lower() or 'group b' in s.lower():
         return None
     return s
+
 
 # a wrapper for openai.Completion.create
 # if the API call fails, it will retry for max_repeat times
@@ -96,50 +117,85 @@ def gpt3wrapper(max_repeat=20, **arguments):
     return None
 
 
+def chatgpt_wrapper(max_repeat=5, **arguments):
+    i = 0
+    while i < max_repeat:
+        try:
+            response = client.chat.completions.create(
+                model=arguments['engine'],
+                messages=[
+                    {
+                        "role": "user",
+                        "content": arguments['prompt']
+                    }
+                ],
+                max_completion_tokens=arguments['max_tokens'],
+                top_p=arguments['top_p'],
+                n=arguments['n']
+            )
+            return response
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt
+        except Exception as e:
+            print(e)
+            print(arguments['prompt'])
+            print('now sleeping for %d seconds...' % SLEEP_TIME_AFTER_ERROR)
+            print('=' * 80)
+            time.sleep(SLEEP_TIME_AFTER_ERROR)
+            i += 1
+    return None
+
+
 class GPT3_Proposer:
 
-    def __init__(self, problem, use_default_hypotheses=False, single_max_length=SINGLE_SAMPLE_MAX_LENGTH, engine_name='text-davinci-003', temperature=0.7):
+    def __init__(self, problem, use_default_hypotheses=False, single_max_length=SINGLE_SAMPLE_MAX_LENGTH,
+                 engine_name='gpt-4o', temperature=0.7):
         if use_default_hypotheses:
             self.example_hypotheses = DEFAULT_HYPOTHESES
         else:
             self.example_hypotheses = (problem['example_hypotheses'] + DEFAULT_HYPOTHESES)[:3]
-        
+
         self.problem = problem
         self.prompt_template = open('templates/gpt3_proposer.txt', 'r').read()
         self.single_max_length = single_max_length
         self.engine_name = engine_name
         self.temperature = temperature
-    
+
     def propose_hypotheses(self, X_A: List[str], X_B: List[str]):
         X_A = [prefix_subspan(x) for x in X_A]
         X_B = [prefix_subspan(x) for x in X_B]
 
-        num_incontext_samples = 25
+        num_incontext_samples = 5
         prompt = None
         arg_dict = {
             k: self.problem[k] for k in ['dataset_description', 'generation', 'A_desc', 'B_desc', 'user', 'target']
         }
         random.shuffle(self.example_hypotheses)
         for i, hypothesis in enumerate(self.example_hypotheses):
-            arg_dict[f'example_hypothesis_{i+1}'] = hypothesis
+            arg_dict[f'example_hypothesis_{i + 1}'] = hypothesis
 
-        while num_incontext_samples > 1:
+        # while num_incontext_samples > 1:
+        #
+        #     sent_subset = construct_blocks(X_A, X_B, num_incontext_samples=num_incontext_samples)
+        #
+        #     A_block, B_block = sent_subset['A_block'], sent_subset['B_block']
+        #     tmp_arg_dict = deepcopy(arg_dict)
+        #     tmp_arg_dict['A_block'] = A_block
+        #     tmp_arg_dict['B_block'] = B_block
+        #     prompt = self.prompt_template.format(**tmp_arg_dict)
+        #     prompt_length = len(GPT3_TOK.encode(prompt))
+        #     if prompt_length < MAX_PROMPT_LENGTH:
+        #         break
+        #     else:
+        #         # prompt too long, reducing num_incontext_samples
+        #         print(num_incontext_samples)
+        #         num_incontext_samples -= 1
 
-            sent_subset = construct_blocks(X_A, X_B, num_incontext_samples=num_incontext_samples)
-            
-            A_block, B_block = sent_subset['A_block'], sent_subset['B_block']
-            tmp_arg_dict = deepcopy(arg_dict)
-            tmp_arg_dict['A_block'] = A_block
-            tmp_arg_dict['B_block'] = B_block
-            prompt = self.prompt_template.format(**tmp_arg_dict)
-            prompt_length = len(GPT3_TOK.encode(prompt))
-            if prompt_length < MAX_PROMPT_LENGTH:
-                break
-            else:
-                # prompt too long, reducing num_incontext_samples
-                print(num_incontext_samples)
-                num_incontext_samples -= 1
+        # arg_dict['A_block'] = sent_subset['A_block']
+        # arg_dict['B_block'] = sent_subset['B_block']
+        # prompt = self.prompt_template.format(**arg_dict)
 
+        sent_subset = construct_blocks(X_A, X_B, num_incontext_samples=num_incontext_samples)
         arg_dict['A_block'] = sent_subset['A_block']
         arg_dict['B_block'] = sent_subset['B_block']
         prompt = self.prompt_template.format(**arg_dict)
@@ -153,9 +209,12 @@ class GPT3_Proposer:
             'n': 1
         }
 
-        result = gpt3wrapper(**query_args)
+        # print(query_args)
 
-        returned_text = result['choices'][0]['text']
+        # result = gpt3wrapper(**query_args)
+        # returned_text = result['choices'][0]['text']
+        result = chatgpt_wrapper(**query_args)
+        returned_text = result.choices[0].message.content
 
         hs = []
         for h in returned_text.split('\n\n')[0].split('\n-'):
--- validator.py
+++ validator.py
@@ -12,6 +12,7 @@ YES_NO_TOK_IDX = [150, 4273]
 MAX_SOURCE_LENGTH = 1024
 TEMPERATURE = 0.001
 
+
 # if you have multiple GPUs, you can parallelize the T5 model across them
 # future packages version will probably have more efficient ways to do this
 def parallelize_across_device(model):
@@ -33,12 +34,15 @@ def parallelize_across_device(model):
 
 
 DEFAULT_VALIDATOR_TEMPLATE = open('templates/t5_validator.txt', 'r').read()
+
+
 class Validator:
 
     # model_path is the path to the T5 model weights used for validation
     # can also any other model name
     # the default is the best model we have trained
-    def __init__(self, model_path: str ='ruiqi-zhong/d5_t5_validator', batch_size: int = BATCH_SIZE, verbose: bool = False, template: str = DEFAULT_VALIDATOR_TEMPLATE):
+    def __init__(self, model_path: str = 'ruiqi-zhong/d5_t5_validator', batch_size: int = BATCH_SIZE,
+                 verbose: bool = False, template: str = DEFAULT_VALIDATOR_TEMPLATE):
         self.tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-xxl")
         print('loading model weights')
         self.model = T5ForConditionalGeneration.from_pretrained(model_path)
@@ -47,7 +51,7 @@ class Validator:
         self.validator_template = template
         self.batch_size = batch_size
         self.verbose = verbose
-    
+
     # input_dicts is a list of dictionaries, each dictionary has two keys: "hypothesis" (h) and "text" (x), mapping to the hypothesis and text to be validated
     # returns a list of scores, each score is a float between 0 and 1, corresponding to the probability that the hypothesis is true given the text for each input dictionary
     # note that it is an iterator, so you can use it in a for loop and save the results whenever some input dictionaries are processed
@@ -56,7 +60,7 @@ class Validator:
         for i, input_dict in enumerate(input_dicts):
             hypothesis, text = input_dict['hypothesis'], input_dict['text']
             prompts.append(self.validator_template.format(hypothesis=hypothesis, text=text))
-        
+
         with torch.no_grad():
             self.model.eval()
             num_batches = (len(prompts) - 1) // self.batch_size + 1
@@ -67,13 +71,13 @@ class Validator:
                 pbar = range(num_batches)
 
             for batch_idx in pbar:
-                input_prompts = prompts[batch_idx * self.batch_size : (batch_idx + 1) * self.batch_size ]
-                inputs = self.tokenizer(input_prompts, 
-                                    return_tensors="pt",
-                                    padding="longest",
-                                    max_length=MAX_SOURCE_LENGTH,
-                                    truncation=True,
-                                    ).to(device)
+                input_prompts = prompts[batch_idx * self.batch_size: (batch_idx + 1) * self.batch_size]
+                inputs = self.tokenizer(input_prompts,
+                                        return_tensors="pt",
+                                        padding="longest",
+                                        max_length=MAX_SOURCE_LENGTH,
+                                        truncation=True,
+                                        ).to(device)
                 generation_result = self.model.generate(
                     input_ids=inputs["input_ids"],
                     attention_mask=inputs["attention_mask"],
@@ -83,32 +87,34 @@ class Validator:
                     return_dict_in_generate=True,
                     output_scores=True
                 )
-                scores = sm(generation_result.scores[0][:,YES_NO_TOK_IDX])[:,1].detach().cpu().numpy().tolist()
+                scores = sm(generation_result.scores[0][:, YES_NO_TOK_IDX])[:, 1].detach().cpu().numpy().tolist()
                 for s in scores:
                     yield s
+
 
 class DummyValidator:
 
     def __init__(self):
         print('!!!!! WARNING: You are using a dummy verifier that returns random results!!!!!!!')
         pass
-    
+
     def validate_w_scores(self, ind_dicts):
         for _ in range(len(ind_dicts)):
             yield 0.01
 
 
 if __name__ == '__main__':
-    input_dicts = [ 
-        {'hypothesis': 'is a positive review', 'text': 'I like this movie.'}, 
+    input_dicts = [
+        {'hypothesis': 'is a positive review', 'text': 'I like this movie.'},
         {'hypothesis': 'is a positive review', 'text': 'I hate this movie.'}
     ]
     input_dicts = input_dicts * 100
-    validator = Validator('../workflow/mount/models/0221distill_verifier_google-flan-t5-xl/checkpoint-10000/')
+    validator = Validator('ruiqi-zhong/d5_t5_validator_3B')
     all_results = []
     for s in validator.validate_w_scores(input_dicts):
         all_results.append(s)
     import numpy as np
+
     all_results = np.array(all_results)
     pred = (all_results > 0.5).astype(int)
     gold = np.array([1, 0] * 100)
